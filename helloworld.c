c5caf0381683e62a216ac3cd06d492335e9486e0
HoRiWo <horiok@iharamfg.co.jp>
2022-06-13 09:32:05 +0900

    20220613_lesson1

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("hello,world\n");
+    printf("\"hello,world\"\n");
+    int x;
+    double y;
+    x = 1;
+    y = 0.5;
+    printf("x = %d\n", x);
+    printf("y = %f\n", y);
+    printf("y = %f\n", x + y);
+}
